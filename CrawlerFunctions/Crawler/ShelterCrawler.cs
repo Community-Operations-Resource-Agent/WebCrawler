2137f63a9e0030ccb87834116c3ca2517c07a9fd
Divyang Dalal <didalal@microsoft.com>
2020-07-28 19:50:58 +0530

    initial commit for shelter

---
+++ CrawlerFunctions/Crawler/ShelterCrawler.cs
@@ -0,0 +1,39 @@
+﻿namespace CrawlerFunctions.Crawler
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using System;
+    using CrawlerFunctions.Models;
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Chrome;
+
+    /// <summary>
+    /// Crawls shelter websites
+    /// </summary>
+    public static class ShelterCrawler
+    {
+        public static IList<ListingInformation> CrawlShelterWebSite(String url, ChromeDriver driver)
+        {
+            IList<ListingInformation> stateList = new List<ListingInformation>();
+            driver.Navigate().GoToUrl(@url);
+
+            IWebElement tableElement = driver.FindElement(By.TagName("table"));
+            IList<IWebElement> tableRow = tableElement.FindElements(By.TagName("tr"));
+            foreach (IWebElement row in tableRow)
+            {
+                IList<IWebElement> rowTD = row.FindElements(By.XPath("//td/a"));
+                foreach (IWebElement td in rowTD)
+                {
+                    ListingInformation state = new ListingInformation
+                    {
+                        Name = td.Text,
+                        Url = td.GetAttribute("href")
+                    };
+                    stateList.Add(state);
+                }
+
+            }
+            return stateList;
+        }
+    }
+}
---
+++ CrawlerFunctions/Providers/IDatastoreProvider.cs
@@ -0,0 +1,33 @@
+﻿namespace CrawlerFunctions.Providers
+{
+    using System;
+
+    using System.Threading.Tasks;
+    using Microsoft.Azure.Documents;
+    using Microsoft.Azure.Documents.Client;
+
+    /// <summary>
+    /// Datastore provider
+    /// </summary>
+    public interface IDatastoreProvider
+    {
+        /// <summary>
+        /// Inserts document in datastore
+        /// </summary>
+        /// <param name="documentCollectionUri"></param>
+        /// <param name="document"></param>
+        /// <param name="options"></param>
+        /// <returns></returns>
+        Task<Document> InsertDocumentAsync(Uri documentCollectionUri, object document, RequestOptions options);
+
+        /// <summary>
+        /// Upserts document in datastore
+        /// </summary>
+        /// <param name="documentUri"></param>
+        /// <param name="document"></param>
+        /// <param name="options"></param>
+        /// <returns></returns>
+        Task<Document> UpsertDocumentAsync(Uri documentUri, object document, RequestOptions options);
+
+    }
+}
