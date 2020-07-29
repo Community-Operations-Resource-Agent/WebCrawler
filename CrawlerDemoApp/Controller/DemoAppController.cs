f213441f6569a77099f6169a24511b3f0d0c4a21
Hong Li <hongli@microsoft.com>
2020-07-29 01:53:23 -0700

    add ASP.NET Core Web Application for Demo

---
+++ CrawlerDemoApp/Controller/DemoAppController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CrawlerDEmoApp.Controllers
+{
+    public class DemoAppController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult CalPage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Crawl()
+        {
+            try
+            {
+            }
+            catch (Exception)
+            {
+                ViewBag.Result = "Crawl";
+            }
+            return View("CrawlerDemo");
+        }
+
+        [HttpPost]
+        public IActionResult Query()
+        {
+            try
+            {
+            }
+            catch (Exception)
+            {
+                ViewBag.Result = "Query";
+            }
+            return View("CrawlerDemo");
+        }
+
+        [HttpPost]
+        public IActionResult Test()
+        {
+            try
+            {
+            }
+            catch (Exception)
+            {
+                ViewBag.Result = "Test";
+            }
+            return View("CrawlerDemo");
+        }
+
+        [HttpPost]
+        public IActionResult DeleteDB()
+        {
+            try
+            {
+            }
+            catch (Exception)
+            {
+                ViewBag.Result = "DeleteDB";
+            }
+            return View("CrawlerDemo");
+        }
+    }
+}
