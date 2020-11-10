909c7b87ee0951b8f4b430b5c03e7fddbe3a693e
Divyang Dalal <didalal@microsoft.com>
2020-11-10 23:20:45 +0530

    Added service bus provider

--- CrawlerFunctions/CrawlPage.cs
+++ CrawlerFunctions/CrawlPage.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Threading.Tasks;
+using CrawlerFunctions.Common;
 using CrawlerFunctions.Crawler;
+using CrawlerFunctions.Providers;
 using HtmlAgilityPack;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
@@ -14,9 +16,15 @@ namespace CrawlerFunctions
     {
         private readonly ScrapingBrowser browser;
 
-        public CrawlPage(ScrapingBrowser b)
+        private readonly IServiceBusProvider _serviceBusProvider;
+
+        private readonly IDatastoreProvider _datastoreProvider;
+
+        public CrawlPage(ScrapingBrowser b, IServiceBusProvider serviceBusProvider, IDatastoreProvider datastoreProvider)
         {
             browser = b;
+            this._serviceBusProvider = serviceBusProvider;
+            this._datastoreProvider = datastoreProvider;
         }
 
         [FunctionName("CrawlPage")]
@@ -34,12 +42,13 @@ namespace CrawlerFunctions
             if (crawlConfig.Level >= crawlConfig.Selectors.Length - 1)
             {
                 // Links:  Parse the URLs, Names and push back on to the queue
+                //_serviceBusProvider.SendMessageAsync(CrawlerUtils.CreateMessage());
             }
             else
             {
                 // If we're not at the last level, we're looking for names & links
                 // Gather all the data and push into Cosmos DB
-
+                //_datastoreProvider.InsertDocumentAsync(document);
             }
             
 
---
+++ CrawlerFunctions/Providers/ServiceBusProvider.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Azure.ServiceBus;
+
+namespace CrawlerFunctions.Providers
+{
+    public class ServiceBusProvider : IServiceBusProvider
+    {
+        private readonly IQueueClient _queueClient;
+
+        public ServiceBusProvider(string serviceBusConnectionString, string queueName)
+        {
+            this._queueClient = new QueueClient(serviceBusConnectionString, queueName, ReceiveMode.PeekLock,
+                RetryPolicy.Default);
+        }
+
+        public void SendMessageAsync(Message message)
+        {
+            this._queueClient.SendAsync(message);
+        }
+    }
+}
